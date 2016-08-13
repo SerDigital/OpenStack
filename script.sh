c72b53a30e755bebf35d3ce8c2fbcd376880364a
Ser Digital <mariodamiancas@yahoo.com.mx>
2016-08-13 14:11:32 -0500

    Actualización de sh

--- script.sh
+++ script.sh
@@ -6,12 +6,11 @@ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libex
 wget https://github.com/SerDigital/OpenStack/Pokemon/index.html
 sudo cp shade-demo.html /var/www/html/index.html
 
+#wget https://github.com/SerDigital/OpenStack/Pokemon/articuno.png
+#sudo cp articuno.png /var/www/html/articuno.png
 
-wget https://github.com/SerDigital/OpenStack/Pokemon/articuno.png
-sudo cp articuno.png /var/www/html/img/articuno.png
+#wget https://github.com/SerDigital/OpenStack/Pokemon/ditto.png
+#sudo cp ditto.png /var/www/html/ditto.png
 
-wget https://github.com/SerDigital/OpenStack/Pokemon/ditto.png
-sudo cp ditto.png /var/www/html/img/ditto.png
-
-wget https://github.com/SerDigital/OpenStack/Pokemon/moltres.png
-sudo cp moltres.png /var/www/html/img/moltres.png
+#wget https://github.com/SerDigital/OpenStack/Pokemon/moltres.png
+#sudo cp moltres.png /var/www/html/moltres.png
