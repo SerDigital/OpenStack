473ba12916514a5dee50349869e9b391e2a988e8
Ser Digital <mariodamiancas@yahoo.com.mx>
2016-08-13 14:30:45 -0500

    Ot

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-wget https://github.com/SerDigital/OpenStack/Pokemon/index.html
+wget https://raw.githubusercontent.com/SerDigital/OpenStack/master/index.html
+
 sudo cp shade-demo.html /var/www/html/index.html
 
 #wget https://github.com/SerDigital/OpenStack/Pokemon/articuno.png
