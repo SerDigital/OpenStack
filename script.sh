93bad0172c25093271fc2d2c45effec80f613f44
Ser Digital <mariodamiancas@yahoo.com.mx>
2016-08-13 14:34:16 -0500

    sss

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libex
 
 wget https://raw.githubusercontent.com/SerDigital/OpenStack/master/index.html
 
-sudo cp shade-demo.html /var/www/html/index.html
+sudo cp index.html /var/www/html/index.html
 
 #wget https://github.com/SerDigital/OpenStack/Pokemon/articuno.png
 #sudo cp articuno.png /var/www/html/articuno.png
